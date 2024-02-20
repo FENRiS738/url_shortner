c34436194a87e22bab65b02dbefaf505ec919b08
FENRiS738 <as420738@gmail.com>
2024-02-20 20:59:13 +0530

    Complete setup of application

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "mongodb+srv://as420738:VVOfwmMLPOkzGmdd@cluster0.e1zoimv.mongodb.net/?retryWrites=true&w=majority"
+
+    class Config:
+        env_file = '.env'
+
+        
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for: {settings.env_name}")
+    return settings
