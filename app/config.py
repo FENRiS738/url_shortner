c83ec1cba1b92d3231aae22b22e22813bd5e8741
FENRiS738 <as420738@gmail.com>
2024-03-24 17:16:55 +0530

    Integrated database, define model and schema

--- app/config.py
+++ app/config.py
@@ -4,7 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
-    db_url: str = "mongodb+srv://as420738:VVOfwmMLPOkzGmdd@cluster0.e1zoimv.mongodb.net/?retryWrites=true&w=majority"
+    db_url: str = "mysql://localhost:3306"
+    # db_url: str = "mongodb+srv://as420738:VVOfwmMLPOkzGmdd@cluster0.e1zoimv.mongodb.net/?retryWrites=true&w=majority"
+    db_name: str = "url_shortner"
 
     class Config:
         env_file = '.env'
--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, status, HTTPException
+
+from . import schemas
 
 app = FastAPI()
 
+def raise_bad_request(message):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=message)
+
 @app.get('/')
-def read_root():
-    return "Welcome to the URL shortener API!"
+def read_root() -> dict:
+    return {
+        'message' : 'Server running successfully!',
+        'status' : status.HTTP_200_OK
+    }
+
+
+@app.post('/url')
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message='Your provided URL is not valid!')
+    return {
+        'status' : status.HTTP_201_CREATED,
+        'message' : f'TODO: Create URL database entry {url.target_url}'
+    }
